5adf51a65aa87f36f3ee906fcbaae48547673696
ksfallon <fallon.katelin@gmail.com>
2021-07-08 17:04:54 -0400

    added notes to the 'routes/apiRoutes' section about what our different routes could look like.put notes for all of the GETs and POSTs we need plus optional DELETEs and PUTs. I also mention how the functions are actually written in the 'controllor/appController.js' file for security purposes when grabbing the user's data

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,6 +14,54 @@ module.exports = (passport, db) => {
   router.post('/user/confirm', AuthController.confirmAuth);
 
   // App
+  // HERE IS WHERE WE NEED TO ADD OUR API ROUTES:
+  // Don't think we need to do many more CRUD for USER bc its under AUTHENTICATION
+  // from each user's homepage do they have a link to each trip they've created based
+
+  // LOCATIONS:
+  // GET User's Location's
+  // POST all of User's Location's
+  // GET specific Location based on location id
+  // POST specific Location based on location id with all
+  // of their places to visit ('parks', 'trails', 'musuems', restaurant').
+  // (maybe add a DELETE here to remove a location?)
+  // (maybe add a PUT here to update location parameters like date?)
+
+  // RESTAURANTS
+  // GET restaurants based on location id
+  // POST restaurant based on location id
+  // GET specific restaurant based on restaurant id
+  // POST specific restaurant based on restaurant id
+  // (maybe add a DELETE here to remove a restaurant?)
+
+  // TRAILS
+  // GET trails based on location id
+  // POST trails based on location id
+  // GET specific trails based on trails id
+  // POST specific trails based on trails id
+  // (maybe add a DELETE here to remove a trails?)
+
+  // PARKS
+  // GET parks based on location id
+  // POST parks based on location id
+  // GET specific parks based on parks id
+  // POST specific parks based on parks id
+  // (maybe add a DELETE here to remove a parks?)
+
+  // MUSEUMS
+  // GET museums based on location id
+  // POST museums based on location id
+  // GET specific museum based on museums id
+  // POST specific museum based on museum id
+  // (maybe add a DELETE here to remove a museum?)
+
+  // 2 routes, from the 'search' page to first GET trip ideas based on location with the parameters
+  // of: dates and parks, trails, museums and restaurants.
+  // And then POST all of this info
+
+  // These example GET, POST and DELETE are connected to 'controllers/appController'
+  // to keep the data gathered secured. So most of the CRUD route functions are actually
+  // written there
   router.get('/examples', AppController.getExamples);
   router.post('/examples', AppController.createExample);
   router.delete('/examples/:id', AppController.deleteExample);
