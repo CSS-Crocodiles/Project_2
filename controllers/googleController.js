98e57456e71591b830184690a27b334bad3fa5bd
Bryan Civils <hermanbciv@gmail.com>
2021-07-12 21:15:57 -0400

    Fully implement front-to-back-to-front client-Google Places call/response

--- controllers/googleController.js
+++ controllers/googleController.js
@@ -4,8 +4,6 @@ module.exports = function () {
   return {
     getTrip: async function (req, res) { // <-- TO DO:  delete function if it proves unneccessary
       try {
-        // const cityIncoming = '';
-        // const stateIncoming = '';
         const neighborhood = 'chelsea';
         const borough = 'manhattan';
         const city = 'new+york+city';
@@ -24,15 +22,10 @@ module.exports = function () {
         const getCity = req.body.city;
         const getState = req.body.state;
         const urlResponse = await axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${getCity}+${getState}&type=restaurant&radius=5000&strictbounds&key=${process.env.GOOGLE_MAPS_KEY}`);
-        console.log(`--> getTripAlt - RESPONSE data:  ${urlResponse.data}`);
-        // res.json();
-        // res.send(data); // this works for Postman
-        res.send(urlResponse.data);
-        // console.log(`--> getTripAlt - RESPONSE JSON DATA:  ${res.json()}`);
-        // console.log(`--> getTripAlt - RESPONSE send DATA:  ${res.send(data)}`);
-        // return res.json(data);
+        console.log(`--> getTripAlt - RESPONSE data:  ${JSON.stringify(urlResponse.data.results)}`);
+        res.json(urlResponse.data); // <-- this works for now
       } catch (err) {
-        console.log('ERROR IN CATCH', err);
+        console.log('Request for Places failed', err);
       }
     }
   };
--- public/assets/js/places.js
+++ public/assets/js/places.js
@@ -1,4 +1,5 @@
-// const googleController = require('../../../controllers/googleController');
+// const { response } = require('express'); // <-- TO DO:  Delete if not needed
+                                            //             It's unclear why it's causing error on load
 const requestOptions = {
   method: 'GET',
   redirect: 'follow'
@@ -6,47 +7,27 @@ const requestOptions = {
 
 console.log(`--> places.js IS RUNNING`);
 
-const newTripGet = async (event) => {
-  event.preventDefault();
-  const responseRes = await fetch(`/api/getGoogleData`, requestOptions);
-  console.log(`--> newTripGet:  ${responseRes}`);
-};
 const newTripHandler = async (event) => {
   event.preventDefault();
   console.log(`--> SUBMIT button was clicked`);
   const city = document.querySelector('#example-text').value.trim();
   const state = document.querySelector('#example-description').value.trim();
-  // const needed_funding = document.querySelector('#project-funding').value.trim();
-  // const description = document.querySelector('#project-desc').value.trim();
-  /*
-      if (name && needed_funding && description) {
-        const response = await fetch(`/api/projects`, {
-          method: 'POST',
-          body: JSON.stringify({ name, needed_funding, description }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
-      */
   if (city && state) {
-    const responseReq = await fetch(`/api/getGoogleData`, {
+    fetch(`/api/getGoogleData`, {
       method: 'POST',
       body: JSON.stringify({ 'city': city, 'state': state }),
       headers: {
         'Content-Type': 'application/json'
       }
+    }).then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      console.log('RESPONSE DATA - ALL:  ', data);
+      console.log('RESPOSNE DATA - FIRST RESULT, NAME:  ', data.results[0].name);
+      // functionHere(data.results) <-- function to loop through data could go here or in separate function
     });
-    if (responseReq.ok) {
-      console.log(`--> responseReq STATUS:  ${responseReq.status}`);
-      // const responseData = await responseReq.json();
-      // console.log(`--> RESPONSE (responseData VARIABLE RETURNING JSON?:  ${responseData})`);
-      console.log(`--> RESPONSE (responseReq raw):  ${responseReq}`);
-      console.log(`--> RESPONSE (JSON.stringify(responseReq)):  ${JSON.stringify(responseReq)}`);
-      // document.location.replace('/profile');
-    } else {
-      alert('Failed to work');
-    }
   }
 };
-  // document.querySelector('.form-group').addEventListener('submit', newTripHandler);
+
+// document.querySelector('.form-group').addEventListener('submit', newTripHandler);
 document.querySelector('#submit').addEventListener('click', newTripHandler);
