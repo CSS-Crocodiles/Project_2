d1cde459714ecdd6328483327bf59859afd03110
William Crews <crewswh@gmail.com>
2021-07-15 14:09:53 -0400

    node..?

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -170,7 +170,7 @@ $('#add-location').on('click', function (event) {
 });
 
 
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, options);
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.sidenav');
+//   var instances = M.Sidenav.init(elems, options);
+// });
