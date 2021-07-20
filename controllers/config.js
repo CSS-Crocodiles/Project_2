37e7ec2e5cdb5455eac4cd59ce272602fe650ad8
William Crews <crewswh@gmail.com>
2021-07-20 18:34:09 -0400

    layout's'

--- controllers/config.js
+++ controllers/config.js
@@ -1,10 +1,7 @@
 const router = require('express').Router();
-const path = require('path');
 
-// This is the 'get' route 
 router.get('/', async (req, res) => {
-  // Here, index.html is rendered
-  res.render('dashboard')
+  res.render('dashboard');
 });
 
 // router.get('/', async (req, res) => {
